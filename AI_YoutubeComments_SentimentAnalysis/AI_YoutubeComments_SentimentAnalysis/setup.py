95adc273db5e9ea0864dd18e6719566d04d67906
Marco Mungai Coppolino <marcomungaicoppolino@gmail.com>
2023-04-16 17:53:46 +0200

    commit

--- AI_YoutubeComments_SentimentAnalysis/AI_YoutubeComments_SentimentAnalysis/setup.py
+++ AI_YoutubeComments_SentimentAnalysis/AI_YoutubeComments_SentimentAnalysis/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-VERSION = '0.0.7'
+VERSION = '0.0.1'
 DESCRIPTION = 'A Classification Project'
 LONG_DESCRIPTION = """The project is a comparison of the performance 
     of an SVM model and a MLP model"""
 
 setup(
-    name='Wine_Quality',
+    name='AI_YoutubeComments_SentimentAnalysis',
     version=VERSION,
     author='Marco Mungai Coppolino',
     author_email="<marcomungaicoppolino@gmail.com>",
@@ -15,14 +15,6 @@ setup(
     long_description=LONG_DESCRIPTION,
     install_requires=['numpy', 'pandas', 'sklearn'],
     keywords=['python', 'data science', 'preprocessing', 'encoding'],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ],
-    packages=['Wine_Quality']
+    packages=['AI_YoutubeComments_SentimentAnalysis']
     
 )
